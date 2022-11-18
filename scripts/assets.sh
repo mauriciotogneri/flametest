c27b1da6fda413fe72fe06a662f95b3d3cc5a2b0
Mauricio Togneri <mauricio.togneri@gmail.com>
2022-11-18 15:29:12 +0100

    Improvements

---
+++ scripts/assets.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+flutter clean
+flutter pub upgrade
+flutter pub pub run daassets:daassets.dart ./pubspec.yaml ./lib/utils/assets.dart
+sed -i'.original' -e 's/\.\/assets/assets/' "./lib/utils/assets.dart"
+sed -i'.original' -e 's/-/_/' "./lib/utils/assets.dart"
+rm "./lib/utils/assets.dart.original"
+flutter format lib
